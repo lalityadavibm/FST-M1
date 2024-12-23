b8073c6e452a13252d618548f540a395e3e80e8f
lalityadavibm <lalit.yadav@ibm.com>
2024-12-23 10:49:57 +0530

    Add files via upload

---
+++ Java/Activities/Activity13.java
@@ -0,0 +1,17 @@
+package Session4;
+
+import java.util.Scanner;
+
+public class Activity13 {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		String array=input.nextLine();
+		
+		System.out.println("Array Number : " + array);
+
+
+	}
+
+}
