aca095a0706d5d0c6b18868d7e09966585b2c75b
lalityadavibm <lalit.yadav@ibm.com>
2025-01-01 10:40:18 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,6 @@
+
+name= input("Please Enter your name: ")
+age= int(input("Please Enter your age: "))
+year=str((2024-age)+100)
+
+print(name + " will be 100 years old in the year " + year)
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,7 @@
+
+number=int(input("Enter a number: "))
+
+if number%2==0:
+    print("Your input an even Number ")
+else:
+    print("Youe input an odd Number")
